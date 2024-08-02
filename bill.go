b71518e2973e05476786652365ee4ce27499186b
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-02 12:58:56 +0100

    Feat: Bill formatter method complete.

--- bill.go
+++ bill.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type bill struct {
 	name  string
 	items map[string]float64
@@ -19,5 +21,17 @@ func newBill(name string) bill {
 
 // format the bill
 func (b bill) format() string {
+	fs := "Bill Breakdown \n"
+	var total float64 = 0
 
+	// list items
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%v...£%v \n", k+": ", v)
+		total += v
+	}
+
+	//total
+	fs += fmt.Sprintf("%v...£%0.2f", "Total:", total)
+
+	return fs
 }
