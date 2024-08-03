7ce0ea92bd5fd5508b4160a63eb82a77c1337ee2
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-03 09:25:48 +0100

    Feat: getInput method body complete.

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 )
 
 func getInput(prompt string, b *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := b.ReadString('\n')
 
+	return strings.TrimSpace(input), err
 }
 
 func createBill() bill {
