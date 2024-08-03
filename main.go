2fb49e6df25312bbfc2224b80d7130c16f962a97
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-03 09:47:13 +0100

    Feat: Prompt options switch case created and options for add tip and add item are fully functional.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -26,6 +27,42 @@ func createBill() bill {
 }
 
 func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	opt, _ := getInput("Chooes option (a - add item, s - save bill, t - add tip)", reader)
+
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		p, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			fmt.Println("Price must be a number")
+			promptOptions(b)
+		}
+		b.addItem(name, p)
+
+		fmt.Println("Added item :", name, price)
+		promptOptions(b)
+	case "s":
+		tip, _ := getInput("Tip amount (£): ", reader)
+
+		t, err := strconv.ParseFloat(tip, 64)
+		if err != nil {
+			fmt.Println("Tip must be a number")
+			promptOptions(b)
+		}
+		b.updateTip(t)
+
+		fmt.Println("Tip added: ", tip)
+		promptOptions(b)
+	case "t":
+		fmt.Print("You chose a")
+	default:
+		fmt.Print("You did not choose a valid option...")
+		promptOptions(b)
+	}
 
 }
 
