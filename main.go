38d85e7b9b47e07dd8ef4ec9419fdefcea4a9c8f
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-03 09:19:19 +0100

    Feat: Create bill func method logic added to use terminal user input to create a new bill.

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
 
+	fmt.Print("Create a new bill name: ")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+
+	b := newBill(name)
+	fmt.Println("Created the bill - ", b.name)
+
+	return b
 }
 
 func main() {
